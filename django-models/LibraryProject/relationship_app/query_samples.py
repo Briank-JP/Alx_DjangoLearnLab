b85c2d0b45044ed9776c3fdd62d67ea44960dd33
Kaheru <kaherubrian2016@gmail.com>
2025-02-20 21:23:58 +0300

    greated a relationship app file and added new models, and a queryset file to querythe model.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,7 +9,7 @@ def get_books_by_author(author_name):
 # List all books in a library.
 def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    all_books = Book.objects.filter(library=library) #retreve all books in that library
+    all_books = library.books.all() #retreve all books in that library
     return all_books
 
 # Retrieve the librarian for a library.
