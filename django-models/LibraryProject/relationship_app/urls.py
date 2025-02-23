86ab6ad92933772cb0eba682a5893be610c3c9b5
Kaheru <kaherubrian2016@gmail.com>
2025-02-23 23:11:25 +0300

    created user authentication roles

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,9 +3,8 @@ from .views import list_books, LibraryDetialView
 # from .views import L SignUpView, register
 from . import views
 # task 3 views
-from relationship_app.views.admin_view import admin_dashboard
-from relationship_app.views.librarian_view import librarian_dashboard
-from relationship_app.views.member_view import member_dashboard
+from .views import admin_view, librarian_view, member_view
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -14,8 +13,10 @@ urlpatterns = [
     path("register/", views.register, name='register'),
     path("login/", views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path("logout/", views.LogoutView.as_view(template_name='relationship_app/logout.html') ,name="logout"),
+    
     # user authentication views
-    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
-    path('librarian-dashboard/', librarian_dashboard, name='librarian_dashboard'),
-    path('member-dashboard/', member_dashboard, name='member_dashboard'),
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
+   
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .models import Book
 from .models import Library
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 def list_books(request):
@@ -34,4 +35,28 @@ class LoginView(CreateView):
     success_url = reverse_lazy(login)
     template_name = 'relationship_app/login.html'
     
-    
+
+# Role-checking functions
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")  # Ensure this path is present
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")  # Ensure this path is present
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")  # Ensure this path is present
