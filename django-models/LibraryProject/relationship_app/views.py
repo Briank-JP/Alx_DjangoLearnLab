5145e21913cef7ce49bc11069046c0378ee9886e
Kaheru <kaherubrian2016@gmail.com>
2025-02-23 20:17:54 +0300

    created and added user authentications and registration form

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -24,14 +24,14 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, template_name='relationship_app/registration/register.html', name='register')
+            return render(request, template_name='relationship_app/register.html', name='register')
     else:
         form = UserCreationForm()
-        return render(request, template_name='relationship_app/registration/register.html')
+        return render(request, template_name='relationship_app/register.html')
 
 class LoginView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy(login)
-    template_name = 'relationship_app/registration/login.html'
+    template_name = 'relationship_app/login.html'
     
     
