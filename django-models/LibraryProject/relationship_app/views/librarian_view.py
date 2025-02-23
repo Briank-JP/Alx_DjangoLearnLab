67b95d1ea95b4436d4c90728969c68c7685941f8
Kaheru <kaherubrian2016@gmail.com>
2025-02-23 23:17:24 +0300

    created user authentication roles

--- django-models/LibraryProject/relationship_app/views/librarian_view.py
+++ django-models/LibraryProject/relationship_app/views/librarian_view.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
-from django.contrib.auth.decorators import user_passes_test
+# from django.http import HttpResponse
+# from django.contrib.auth.decorators import user_passes_test
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+# def is_librarian(user):
+#     return user.userprofile.role == 'Librarian'
 
-@user_passes_test(is_librarian)
-def librarian_dashboard(request):
-    return HttpResponse("Librarian Dashboard: Only accessible to Librarians.")
+# @user_passes_test(is_librarian)
+# def librarian_dashboard(request):
+#     return HttpResponse("Librarian Dashboard: Only accessible to Librarians.")
