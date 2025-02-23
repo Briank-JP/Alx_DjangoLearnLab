157ad6cf845d65a6f5e8ab9915c175877912ee8a
Kaheru <kaherubrian2016@gmail.com>
2025-02-23 20:15:39 +0300

    created and added user authentications and registration form

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('books/<int:pk>/', LibraryDetialView.as_view(), name='book_detail'),
      path("register/", views.register, name='register'),
-    path("login/", views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path("logout/", views.LogoutView.as_view(template_name='relationship_app/logout.html') ,name="logout"),
+    path("login/", views.LoginView.as_view(template_name='relationship_app/registration/login.html'), name='login'),
+    path("logout/", views.LogoutView.as_view(template_name='relationship_app/registration/logout.html') ,name="logout"),
 ]
