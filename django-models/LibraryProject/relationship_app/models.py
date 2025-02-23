21c99dbb83ba54f592dde688e501ec6882ba3fa2
Kaheru <kaherubrian2016@gmail.com>
2025-02-23 22:59:23 +0300

    created user authentication roles

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,6 +5,52 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+]
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# Signal to create or update UserProfile whenever a User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+                return self.name
+        
+    class Book(models.Model):
+        title = models.CharField(max_length=200)
+        author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    
+        def __str__(self):
+            return self.title
+    
+        class Meta:
+            permissions = [
+                ("can_add_book", "Can add book"),
+                ("can_change_book", "Can change book"),
+                ("can_delete_book", "Can delete book"),
+            ]
+
+
+
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
@@ -30,23 +76,23 @@ class Librarian(models.Model):
     
 # task 3 user roles
 
-class UserProfile(models.Model):
-    ROLE_CHOICES = (
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    )
+# class UserProfile(models.Model):
+#     ROLE_CHOICES = (
+#         ('Admin', 'Admin'),
+#         ('Librarian', 'Librarian'),
+#         ('Member', 'Member'),
+#     )
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+#     def __str__(self):
+#         return f"{self.user.username} - {self.role}"
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.userprofile.save()
