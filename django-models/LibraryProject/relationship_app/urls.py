6f5870d90f341f31a1d6ddb4041b632ba468e717
Kaheru <kaherubrian2016@gmail.com>
2025-02-23 10:31:55 +0300

    added class and function based view and created a url path to connect to the templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import list_books, LibraryDetialView
+from .views import list_books, LibraryDetailView
 
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
-    path('books/<int:pk>/', LibraryDetialView.as_view(), name='book_detail'),
+    path('books/<int:pk>/', LibraryDetailView.as_view(), name='book_detail'),
 ]
