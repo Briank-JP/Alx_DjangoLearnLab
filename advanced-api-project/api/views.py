53d2011214552108b4acba53a8053ffd546fd58f
Kaheru <kaherubrian2016@gmail.com>
2025-03-13 13:50:06 +0300

    added custome generic views and permision and also added endpoints

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics 
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 # use the generic views to perfom crud
@@ -25,9 +25,10 @@ class BookDeleteView(generics.DestroyAPIView):
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
