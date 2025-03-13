9dbe6f1f625780fdf9fe3a2ddff2b31e967b62ec
Kaheru <kaherubrian2016@gmail.com>
2025-03-13 13:47:17 +0300

    added custome generic views and permision and also added endpoints

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,8 +5,8 @@ app_name = 'library'
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book_list'),
-    path('books/', BookCreateView.as_view(), name='book_add'),
-    path('books/<int:pk>', BookDeleteView.as_view(), name='book_delete'),
-    path('update/<int:pk>', BookUpdateView.as_view(), name='book_update'),
+    path('books/create', BookCreateView.as_view(), name='book_add'),
+    path('books/delete/<int:pk>', BookDeleteView.as_view(), name='book_delete'),
+    path('update/update/<int:pk>', BookUpdateView.as_view(), name='book_update'),
     path('detail/<int:pk>', BookDetailView.as_view(), name='book_detail'),
 ]
