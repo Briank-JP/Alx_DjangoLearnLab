125590f1b61acdaa4bceb7a43b706f5f04a22a27
Kaheru <kaherubrian2016@gmail.com>
2025-03-13 13:44:08 +0300

    added custome generic views and permision and also added endpoints

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import BookListView, BookCreateView, BookDeleteView, BookUpdateView, BookDetailView
+
+app_name = 'library'
+
+urlpatterns = [
+    path('books/', BookListView.as_view(), name='book_list'),
+    path('books/', BookCreateView.as_view(), name='book_add'),
+    path('books/<int:pk>', BookDeleteView.as_view(), name='book_delete'),
+    path('update/<int:pk>', BookUpdateView.as_view(), name='book_update'),
+    path('detail/<int:pk>', BookDetailView.as_view(), name='book_detail'),
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+# use the generic views to perfom crud
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permissions = [IsAuthenticated]
+    
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'title'
+    permission = [IsAuthenticated]
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
