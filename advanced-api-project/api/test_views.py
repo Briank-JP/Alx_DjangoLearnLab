bf36a5ba6cbcc8e7dba84d17ed94605cbe8976ba
Kaheru <kaherubrian2016@gmail.com>
2025-03-14 23:06:37 +0300

    testcase for crud oparations

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,13 +1,28 @@
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
 
 # Create your tests here.
 
 class BookListTests(APITestCase):
     def setUp(self):
-        print("setting up....")
+        # create a new user
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        
+    def test_authentcated_user_can_access_book_list(self):
+        # login the user
+        self.client.login(username='testuser', password='testpassword')
+        
+        # make a request 
+        response = self.client.get('/api/books/')
+        
+        # check the response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
+      
         
     def test_book_list(self):
         response = self.client.get("/api/books/")
