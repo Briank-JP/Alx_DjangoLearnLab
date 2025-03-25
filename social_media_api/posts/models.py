adcee31b96bbe381046ebebc62e04162f9109be4
Kaheru <kaherubrian2016@gmail.com>
2025-03-26 01:46:26 +0300

    created a posts app to handle posts and comments functionalities.

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -8,8 +8,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # referencing the user created in the accounts app
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DateField()
-    updated_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
@@ -18,8 +18,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateField()
-    updated_at = models.DateField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
     
     def __str__(self):
         return f"{self.author} commented on {self.post.title}"
