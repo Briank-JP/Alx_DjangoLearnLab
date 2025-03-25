67ee1de761ed08bcbac6d8e10927d57e551cfeca
Kaheru <kaherubrian2016@gmail.com>
2025-03-26 00:33:51 +0300

    impemented user authentications,register and login. Also added token authorization

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # We create a custom user that inherits from django's abstract user and we also included a filed followers that is symetrical set to false meaning user A follows user B, user B doesnt automatically follow user A, and the related name helps us to have access to the users being followed.
 # include the rest framework auth tokens to the setting.py file
 class CustomUser(AbstractUser):
-    bio = models.CharField(max_length=350, null=True)
+    bio = models.TextField(null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
     
