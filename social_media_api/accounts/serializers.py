ddcac6ade49be4f5e5f9ceb6b376d6ca2f31908b
Kaheru <kaherubrian2016@gmail.com>
2025-03-26 01:41:58 +0300

    created a posts app to handle posts and comments functionalities.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,27 +9,27 @@ class CustomUserSerializer(serializers.ModelSerializer):
     
     password = serializers.CharField()
     class Meta:
-        model = get_user_model
+        model = CustomUser
         fields = ['username', 'password','bio', 'email']
           
           
     # the create method will help us get information from the user and use the creat_uer()function to create the user with validated data
-    def create(self, validated_data):
-        
- # required fields are categoried with the [field] while optional field are seen with .get(fiel, default field)
- 
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ""),
-            profile_picture=validated_data.get('profile_picture', None)
-        )
-        token = Token.objects.create(user=user)
-        return {
-            'user': CustomUserSerializer(user).data,
-            'token': token.key,
-        }
+        def create(self, validated_data):
+            
+    # required fields are categoried with the [field] while optional field are seen with .get(fiel, default field)
+    
+            user = get_user_model().objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password'],
+                bio=validated_data.get('bio', ""),
+                profile_picture=validated_data.get('profile_picture', None)
+            )
+            token = Token.objects.create(user=user)
+            return {
+                'user': CustomUserSerializer(user).data,
+                'token': token.key,
+            }
         
 """
 Generally this serialiser file, we are utilizing  the custom user model and  the get_user_model, this will help choose the current user; if itss the default user or our custom user model.
---
+++ social_media_api/posts/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # referencing the user created in the accounts app
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateField()
+    updated_at = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
+    
+    def __str__(self):
+        return f"{self.author} commented on {self.post.title}"
+  
+    
